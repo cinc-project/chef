f5768f497eba32a957819c79f065fccd1333f81a
Lance Albertson <lance@osuosl.org>
2019-06-04 14:04:38 -0700

    Cinc stable patches

--- chef-config/lib/chef-config/dist.rb
+++ chef-config/lib/chef-config/dist.rb
@@ -1,19 +1,19 @@
 module ChefConfig
   class Dist
     # The chef executable name.
-    EXEC = "chef".freeze
+    EXEC = "cinc".freeze
 
     # The client's alias (chef-client)
-    CLIENT = "chef-client".freeze
+    CLIENT = "cinc-client".freeze
 
     # A short name for the product
-    SHORT = "chef".freeze
+    SHORT = "cinc".freeze
 
     # The suffix for Chef's /etc/chef, /var/chef and C:\\Chef directories
     # "cinc" => /etc/cinc, /var/cinc, C:\\cinc
-    DIR_SUFFIX = "chef".freeze
+    DIR_SUFFIX = "cinc".freeze
 
     # The user's configuration directory
-    USER_CONF_DIR = ".chef".freeze
+    USER_CONF_DIR = ".cinc".freeze
   end
 end
