e43c68594084345f3f34e2cfaaf35b35337640e2
Lance Albertson <lance@osuosl.org>
2019-06-04 14:04:38 -0700

    Cinc stable patches

--- chef-utils/lib/chef-utils/dist.rb
+++ chef-utils/lib/chef-utils/dist.rb
@@ -3,59 +3,59 @@ module ChefUtils
   module Dist
     class Apply
       # The chef-apply product name
-      PRODUCT = "Chef Infra Apply".freeze
+      PRODUCT = "Cinc Apply".freeze
 
       # The chef-apply binary
-      EXEC = "chef-apply".freeze
+      EXEC = "cinc-apply".freeze
     end
 
     class Automate
       # name of the automate product
-      PRODUCT = "Chef Automate".freeze
+      PRODUCT = "Cinc Dashboard".freeze
     end
 
     class Infra
       # When referencing a product directly, like Chef (Now Chef Infra)
-      PRODUCT = "Chef Infra Client".freeze
+      PRODUCT = "Cinc Client".freeze
 
       # A short designation for the product, used in Windows event logs
       # and some nomenclature.
-      SHORT = "chef".freeze
+      SHORT = "cinc".freeze
 
       # The client's alias (chef-client)
-      CLIENT = "chef-client".freeze
+      CLIENT = "cinc-client".freeze
 
       # The chef executable, as in `chef gem install` or `chef generate cookbook`
-      EXEC = "chef".freeze
+      EXEC = "cinc".freeze
 
       # The chef-shell executable
-      SHELL = "chef-shell".freeze
+      SHELL = "cinc-shell".freeze
 
       # Configuration related constants
       # The chef-shell configuration file
-      SHELL_CONF = "chef_shell.rb".freeze
+      SHELL_CONF = "cinc_shell.rb".freeze
 
       # The user's configuration directory
-      USER_CONF_DIR = ".chef".freeze
+      USER_CONF_DIR = ".cinc".freeze
 
       # The suffix for Chef's /etc/chef, /var/chef and C:\\Chef directories
       # "chef" => /etc/cinc, /var/cinc, C:\\cinc
-      DIR_SUFFIX = "chef".freeze
+      DIR_SUFFIX = "cinc".freeze
     end
 
     class Org
       # product Website address
-      WEBSITE = "https://chef.io".freeze
+      WEBSITE = "https://cinc.sh".freeze
 
       # The downloads site
-      DOWNLOADS_URL = "downloads.chef.io".freeze
+      DOWNLOADS_URL = "downloads.cinc.sh".freeze
 
       # The legacy conf folder: C:/opscode/chef. Specifically the "opscode" part
       # DIR_SUFFIX is appended to it in code where relevant
-      LEGACY_CONF_DIR = "opscode".freeze
+      LEGACY_CONF_DIR = "cincproject".freeze
 
       # Enable forcing Chef EULA
-      ENFORCE_LICENSE = true
+      ENFORCE_LICENSE = false
 
       # product patents page
       PATENTS = "https://www.chef.io/patents".freeze
@@ -66,32 +66,32 @@ module ChefUtils
 
     class Server
       # The name of the server product
-      PRODUCT = "Chef Infra Server".freeze
+      PRODUCT = "Cinc Server".freeze
 
       # The server's configuration directory
-      CONF_DIR = "/etc/chef-server".freeze
+      CONF_DIR = "/etc/cinc-server".freeze
 
       # The servers's alias (chef-server)
-      SERVER = "chef-server".freeze
+      SERVER = "cinc-server".freeze
 
       # The server's configuration utility
-      SERVER_CTL = "chef-server-ctl".freeze
+      SERVER_CTL = "cinc-server-ctl".freeze
     end
 
     class Solo
       # Chef-Solo's product name
-      PRODUCT = "Chef Infra Solo".freeze
+      PRODUCT = "Cinc Solo".freeze
 
       # The chef-solo executable (legacy local mode)
-      EXEC = "chef-solo".freeze
+      EXEC = "cinc-solo".freeze
     end
 
     class Zero
       # chef-zero executable
-      PRODUCT = "Chef Infra Zero".freeze
+      PRODUCT = "Cinc Zero".freeze
 
       # The chef-zero executable (local mode)
-      EXEC = "chef-zero".freeze
+      EXEC = "cinc-zero".freeze
     end
   end
 end
