0cf9a2376db1a766bfea0809ab517f0a83cb703d
Tensibai Zhaoying <tensibai@gmail.com>
2020-06-10 23:49:47 +0000

    Update hab plan for 16 for patch
    Signed-off-by: Tensibai Zhaoying <tensibai@gmail.com>

--- habitat/config/client.rb
+++ habitat/config/client.rb
@@ -1,4 +1,4 @@
-chef_repo_path "{{pkg.svc_data_path}}/chef"
+chef_repo_path "{{pkg.svc_data_path}}/cinc"
 file_backup_path "{{pkg.svc_data_path}}/{{cfg.file_backup_path}}"
 pid_file "{{pkg.svc_data_path}}/{{cfg.pid_file}}"
 data_collector.server_url "{{cfg.data_collector.url}}"
@@ -10,8 +10,8 @@ local_mode "{{cfg.local_mode}}"
 {{#if cfg.chef-client.node_name ~}}
 node_name "{{cfg.node_name}}"
 {{/if ~}}
-splay "{{cfg.splay}}"
-interval "{{cfg.interval}}"
+splay {{cfg.splay}}
+interval {{cfg.interval}}
 log_location "{{cfg.log_location}}"
 log_level "{{cfg.log_level}}".to_sym
 {{#if cfg.use_member_id_as_uuid ~}}
