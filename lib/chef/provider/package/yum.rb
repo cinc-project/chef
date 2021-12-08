85a4b79d9faa3f0bf0f8fc4446d0073abc414a2f
Lamont Granquist <lamont@scriptkiddie.org>
2021-12-07 16:50:57 -0800

    Revert "just testing"
    This reverts commit ec4c314a8b9244a2500d99275c1b94c16aaf61ae.

--- lib/chef/provider/package/yum.rb
+++ lib/chef/provider/package/yum.rb
@@ -87,7 +87,6 @@ class Chef
         end
 
         def install_package(names, versions)
-          raise "boom"
           method = nil
           methods = []
           names.each_with_index do |n, i|
