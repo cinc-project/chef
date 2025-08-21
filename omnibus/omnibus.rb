b51e5106aaa1350111fab4a5ff1956872228ab69
Lance Albertson <lance@osuosl.org>
2025-08-21 15:38:31 -0700

    Use cinc S3 bucket
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -35,12 +35,12 @@ use_git_caching true
 
 # Enable S3 asset caching
 # ------------------------------
-use_s3_caching ENV.fetch("OMNIBUS_USE_S3_CACHING", true)
+use_s3_caching true
 s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
 s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
-s3_bucket      "opscode-omnibus-cache-private"
-s3_acl         "private"
-s3_region      "us-west-2"
+s3_bucket      "cinc-omnibus-cache"
+s3_endpoint    "https://s3.osuosl.org"
+s3_force_path_style true
 
 build_retries 3
 fetcher_retries 3
